b9f4ea2cee40c5a72dff0a826c4f4e8d59864f3c
minhquang053 <minhquangbui053@gmail.com>
2023-11-01 11:13:57 +0700

    update api

--- api/server.js
+++ api/server.js
@@ -4,19 +4,19 @@ require('dotenv').config();
 
 const app = require('./app');
 const { mongoConnect } = require('./services/mongo');
-const { loadUsersData } = require('./models/users.model');
-const { loadOrdersData } = require('./models/orders.model');
-const { loadTransfersData } = require('./models/transfers.model');
+// const { loadUsersData } = require('./models/users.model');
+// const { loadOrdersData } = require('./models/orders.model');
+// const { loadTransfersData } = require('./models/transfers.model');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
 async function startServer() {
   await mongoConnect();
-  await loadUsersData();
-  await loadOrdersData();
-  await loadTransfersData();
+  // await loadUsersData();
+  // await loadOrdersData();
+  // await loadTransfersData();
   
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
