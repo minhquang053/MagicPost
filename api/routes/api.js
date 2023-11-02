d03fa24ee1431fc306a4c3496395052dd8746758
minhquang053 <minhquangbui053@gmail.com>
2023-11-02 11:20:59 +0700

    add login route

--- api/routes/api.js
+++ api/routes/api.js
@@ -3,11 +3,13 @@ const express = require('express');
 const usersRouter = require('./users/users.router');
 const ordersRouter = require('./orders/orders.router');
 const transfersRouter = require('./transfers/transfers.router');
+const loginRouter = require('./login/login.router');
 
 const api = express.Router();
 
 api.use('/users', usersRouter);
 api.use('/orders', ordersRouter);
 api.use('/transfers', transfersRouter);
+api.use('/login', loginRouter);
 
 module.exports = api;
---
+++ api/routes/login/login.controller.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcrypt');
+
+const { getUserByEmail } = require('../../models/users.model');
+const { createAccessToken } = require('../../services/jwt');
+
+// to replace with environment variable later
+
+async function httpLogin(req, res) {
+    const login = req.body;
+    const user = await getUserByEmail(login.email);
+    bcrypt.compare(login.password, user.password)
+    .then(async valid => {
+        if (valid) {
+            return res.status(200).json({
+                token: createAccessToken(user.role),
+            });
+        } else {
+            return res.status(400).json({
+                error: "Invalid user"
+            })
+        }
+    })
+    .catch(err => {
+        return res.status(500).json({
+            error: "Couldn't validate user"
+        })
+    });
+}
+
+module.exports = {
+    httpLogin,
+}
