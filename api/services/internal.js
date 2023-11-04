50c6f93833e404d912c48c8d1606a981d9ff37e2
minhquang053 <minhquangbui053@gmail.com>
2023-11-05 01:04:51 +0700

    add find transfer path

--- api/services/internal.js
+++ api/services/internal.js
@@ -1,6 +1,7 @@
-const role_orders = ["Admin", "Manager", "Clerk", "Shipper"];
-const all_roles = ["Admin", "Manager", "Clerk", "Shipper", undefined];
+const role_orders = ["Admin", "Manager", "Processor", "Shipper"];
+const all_roles = ["Admin", "Manager", "Processor", "Shipper", undefined];
 const all_locations = ["a1", undefined];
+const all_sendlocs = ["s1", "s2", "s3", undefined];
 
 function lowerRolesThan(role) {
     const lowerRoles = role_orders.slice(role_orders.indexOf(role) + 1);
@@ -18,16 +19,11 @@ function editRolePermissionGranted(reqUser, targetUser) {
     return false;
 }
 
-module.exports = {
-    lowerRolesThan,
-    editRolePermissionGranted,
-};
-
 /*
 Check if any of the fixed information is correct
 If an information is leftout of the function, they will be treated as undefined
 */
-function validateInfo(user) {
+function validateUserInfo(user) {
     if (all_roles.includes(user.role) &&
         all_locations.includes(user.location)) {
         return true;
@@ -35,8 +31,27 @@ function validateInfo(user) {
     return false;
 }
 
+function validateOrderInfo(order) {
+    if (all_sendlocs.includes(order.sendLocation)) {
+        return true;
+    }
+    return false;
+}
+
+/*
+Find cost-effective to transfer an order from the send location 
+to the assemble locations
+*/
+function findTransferPath(order) {
+    // add finding logic if there are middle points between send and receive location
+
+    return [order.sendLocation, order.receiveLocation, "finloc"];
+}
+
 module.exports = {
     lowerRolesThan,
     editRolePermissionGranted,
-    validateInfo,
+    validateUserInfo,
+    validateOrderInfo,
+    findTransferPath
 }
